f39bc691e33f6a26d0e2001fc41c56755544714c
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-11 15:56:48 +0530

    Add useNavigate Hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable */
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
 const links = [
   { path: '/', text: 'Home' },
@@ -8,18 +9,56 @@ const links = [
   { path: 'profile', text: 'Profile' },
   { path: 'login', text: 'Login' },
 ];
-const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      {
-      links.map((link) => (
-        <li key={link.text}>
-          <NavLink to={link.path}>{link.text}</NavLink>
-        </li>
-      ))
-      }
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const { user, logout } = useAuthContext();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+  return (
+    <>
+      <nav className="navbar">
+        <ul>
+          {links.map((link) => (
+            <React.Fragment key={link.text}>
+              {link.path === 'login' ? (
+                !user && (
+                <li>
+                  <NavLink to={link.path}>{link.text}</NavLink>
+                </li>
+                )
+              ) : link.path === 'profile' ? (
+                user && (
+                <li>
+                  <NavLink to={link.path}>
+                    {link.text}
+                  </NavLink>
+                </li>
+                )
+              ) : (
+                <li>
+                  <NavLink to={link.path}>{link.text}</NavLink>
+                </li>
+              )}
+            </React.Fragment>
+          ))}
 
+          {!user && (
+          <li className="log-in">
+            <span>Log in to edit to-dos</span>
+          </li>
+          )}
+        </ul>
+
+      </nav>
+      {user && (
+      <div className="logout">
+        <p>{user}</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      )}
+    </>
+  );
+};
 export default Navbar;
